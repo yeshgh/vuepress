d7eac49b7f3f35501778d22af80ce88d6c9f332c
hys <yesh_h@163.com>
2021-05-27 10:31:18 +0800

    deploy

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'VuePress Blog Example',
   description: 'This is a blog example built by VuePress',
   theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  dest: 'dist/blog',
   themeConfig: {
     /**
      * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
